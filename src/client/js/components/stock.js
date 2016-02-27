8acb306cf5505c407d0cdfdb196d04fd1fbd988d
Charles Shapleigh <CShapleigh@gmail.com>
2016-02-27 11:24:54 -0500

    Flesh out stock API

--- src/client/js/components/stock.js
+++ src/client/js/components/stock.js
@@ -5,12 +5,21 @@ export default class StockComponent extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      value: null
+      lastPrice: null,
+      dayLow: null,
+      dayHigh: null,
+      dayOpen: null,
+      change: null
     }
   }
 
   componentDidMount() {
+    //Not sure how we're going to pass the stock info here
+    stockObj = this.makeRequest(nse);
+    setStockState(stockObj);
+  }
 
+  makeRequest(nse) {
     request({
           url: `stock/${nse}`,
           method: 'GET',
@@ -19,23 +28,29 @@ export default class StockComponent extends Component {
           }
         }, (error, response, body) => {
           const stockObj = JSON.parse(body);
-          this.setState({
-            value: {nse}
-          });
+          return stockObj
         });
-    }
+  }
 
-    jsonToStockArray(stockObj) {
-      //
-      return null;
-    }
+  setStockState(stockObj) {
+    this.setState({
+      lastPrice: stockObj.main.LastPrice.0
+      dayLow: stockObj.main.Low.0
+      dayHigh: stockObj.main.High.0
+      dayOpen: stockObj.main.Open.0
+      change: stockObj.main.Change.0
+    });
+  }
 
-    render() {
-      return (
-        //loop here?
-        <div>
-          Your stock price is: {this.state.value}
-        </div>
-      )
-    }
+  render() {
+    //Not sure relaly how to pass proper HTML into here
+    return (
+      <div>
+        Current Price: {this.state.lastPrice} (this.state.change) //Change to red/green based on pos/neg?
+        Daily Low: {this.state.dayLow}
+        Daily High: {this.state.dayHigh}
+        Open: {this.state.dayOpen}
+      </div>
+    )
+  }
 }
